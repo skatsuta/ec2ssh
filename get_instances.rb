a556b746c3c9a64aa759a19446a411989ed67c06
Soshi Katsuta <soshi.katsuta@gmail.com>
2016-08-17 01:11:39 +0900

    refactor get_instances.rb
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- get_instances.rb
+++ get_instances.rb
@@ -6,62 +6,58 @@ require 'inifile'
 require 'optparse'
 require 'yaml'
 
-CACHE = "#{Dir.home}/.ec2ssh"
+CACHE = "#{Dir.home}/.ec2ssh".freeze
+CACHE_TTL = 300
 
 profile = 'default'
-$opt = {}
+options = {}
 OptionParser.new do |opt|
   opt.banner = "Usage: #{opt.program_name} [options]"
-  opt.on('-h', '--help', 'Show usage') { puts opt.help ; exit }
-  opt.on('-f', '--flush', 'Flush cache') { $opt[:ignore] = true }
-  opt.on('-p PROFILE', '--profile PROFILE', 'Specify profile') { |v|
+  opt.on('-h', '--help', 'Show usage') do
+    puts opt.help
+    exit
+  end
+  opt.on('-f', '--flush', 'Flush cache') { options[:ignore] = true }
+  opt.on('-p PROFILE', '--profile PROFILE', 'Specify profile') do |v|
     profile = "profile #{v}"
     Aws.config[:credentials] = Aws::SharedCredentials.new(profile_name: profile)
-  }
+  end
   opt.parse!(ARGV)
 end
 
-ini = IniFile.load(File.expand_path("~/.aws/config"))
+ini = IniFile.load(File.expand_path('~/.aws/config'))
 region = ENV['REGION'] || ini[profile]['region']
 Aws.config[:region] = region
 
-instances = nil
+instances = []
 # load cache if fresh
-instances = nil
-if File.exist?(CACHE) && $opt[:ignore].nil? then
+if File.exist?(CACHE) && options[:ignore].nil?
   mtime = File::Stat.new(CACHE).mtime
-  if Time.now - mtime < 3600 then
+  if Time.now - mtime < CACHE_TTL
     cache = YAML.load_file(CACHE)
-    instances = cache[profile][region] rescue nil
+    instances = cache[profile][region]
   end
 end
 
-if instances.nil? then
+if instances.empty?
   ec2 = Aws::EC2::Client.new
   instances = ec2.describe_instances.reservations
-  File::open(CACHE, "w") { |f|
-    cache = {}
-    cache[profile] = {}
-    cache[profile][region] = instances
+  File.open(CACHE, 'w') do |f|
+    cache = { profile => { region => instances } }
     YAML.dump(cache, f)
-  }
+  end
 end
 
-if instances
-  instances.each do |reservation|
-    reservation.instances.each do |instance|
-      next unless instance.state.name == "running"
-      user = "ec2-user"
-      name = instance.instance_id
-      dnsname = instance.public_dns_name
-      if dnsname == nil
-        dnsname = instance.private_dns_name
-      end
-      instance.tags.each do |tag|
-        name = tag.value if tag.key =~ /^name/i
-        user = tag.value if tag.key =~ /^user/i
-      end
-      puts "\"#{name}\"\t#{user}@#{dnsname}\t#{instance.instance_id}"
+instances.each do |reservation|
+  reservation.instances.each do |instance|
+    next unless instance.state.name == 'running'
+    user = 'ec2-user'
+    name = instance.instance_id
+    dnsname = instance.public_dns_name || instance.private_dns_name
+    instance.tags.each do |tag|
+      name = tag.value if tag.key =~ /^name/i
+      user = tag.value if tag.key =~ /^user/i
     end
+    puts "\"#{name}\"\t#{user}@#{dnsname}\t#{instance.instance_id}"
   end
 end
