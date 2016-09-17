04e6153bfeaf2d5c25d99f53b04f97d52cc1c4b8
Soshi Katsuta <soshi.katsuta@gmail.com>
2016-09-17 15:15:28 +0900

    save infomation of instances with self-defined struct format
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- get_instances.rb
+++ get_instances.rb
@@ -8,6 +8,7 @@ require 'yaml'
 
 CACHE = "#{Dir.home}/.ec2ssh".freeze
 CACHE_TTL = 3600
+INSTANCE = Struct.new(:tags, :public_dns_name, :private_dns_name, :instance_id)
 
 profile = 'default'
 options = {}
@@ -43,22 +44,26 @@ if instances.empty?
   ec2 = Aws::EC2::Client.new
   instances = ec2.describe_instances(
     filters: [{ name: 'instance-state-name', values: ['running'] }]
-  ).reservations
+  ).reservations.flat_map(&:instances).map! do |instance|
+    INSTANCE.new(instance.tags,
+                 instance.public_dns_name,
+                 instance.private_dns_name,
+                 instance.instance_id)
+  end
   File.open(CACHE, 'w') do |f|
     cache = { profile => { region => instances } }
     YAML.dump(cache, f)
   end
 end
 
-instances.each do |reservation|
-  reservation.instances.each do |instance|
-    user = 'ec2-user'
-    name = instance.instance_id
-    dnsname = instance.public_dns_name || instance.private_dns_name
-    instance.tags.each do |tag|
-      name = tag.value if tag.key =~ /^name/i
-      user = tag.value if tag.key =~ /^user/i
-    end
-    puts "\"#{name}\"\t#{user}@#{dnsname}\t#{instance.instance_id}"
+list = instances.map do |instance|
+  user = 'ec2-user'
+  name = instance.instance_id
+  dnsname = instance.public_dns_name || instance.private_dns_name
+  instance.tags.each do |tag|
+    name = tag.value if tag.key =~ /^name/i
+    user = tag.value if tag.key =~ /^user/i
   end
-end
+  "\"#{name}\"\t#{user}@#{dnsname}\t#{instance.instance_id}"
+end.join("\n")
+puts list
